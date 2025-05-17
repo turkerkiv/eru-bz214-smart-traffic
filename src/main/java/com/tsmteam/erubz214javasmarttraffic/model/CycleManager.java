569b212769e070bd650cac5df8aed056e4bffeb5
turkerkiv <kivturker@gmail.com>
2025-05-17 14:50:56 +0300

    add: functionality to create new cycle method

--- src/main/java/com/tsmteam/erubz214javasmarttraffic/model/CycleManager.java
+++ src/main/java/com/tsmteam/erubz214javasmarttraffic/model/CycleManager.java
@@ -1,18 +1,38 @@
-package main.java.com.tsmteam.model;
+package com.tsmteam.erubz214javasmarttraffic.model;
 
+import com.tsmteam.erubz214javasmarttraffic.enums.Direction;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Rectangle;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class CycleManager {
-    public final int CYCLE_DURATION = 120;
-    public final int YELLOW_DURATION = 1;
-    List<TrafficLight> _trafficLights;
+    public static final int CYCLE_DURATION = 120;
+    public static final int YELLOW_DURATION = 1;
+    private static List<TrafficLight> _trafficLights;
+
+    // there will be one background images pane, one lights pane, one cars pane
 
     public CycleManager() {
-        createNewCycle(new int[] {4, 3, 5, 1, 2});
     }
 
-    public void createNewCycle(int[] carCounts) {
-        // i guess must be in shape of [n of lights, first light, second, third, forth]
+    public static void createNewCycle(Rectangle northRoad, Rectangle eastRoad, Rectangle southRoad, Rectangle westRoad, int[] carCounts, Pane vehiclesPane) {
+        // lets say right now carCounts in shape of [north, east, south, west]
+        // absolutely change the signature of this method to make it more readable
+        _trafficLights = new ArrayList<>();
+        _trafficLights.add(new TrafficLight(Direction.NORTH, northRoad));
+        _trafficLights.add(new TrafficLight(Direction.EAST, eastRoad));
+        _trafficLights.add(new TrafficLight(Direction.SOUTH, southRoad));
+        _trafficLights.add(new TrafficLight(Direction.WEST, westRoad));
+
+        int totalCars = Arrays.stream(carCounts).sum();
+        for (int i = 0; i < carCounts.length; i++) {
+            TrafficLight light = _trafficLights.get(i);
+            light.calculateGLDuration(totalCars, CYCLE_DURATION);
+            light.addVehiclesToLine(VehicleCreator.createVehicles(carCounts[i], vehiclesPane));
+        }
     }
 
     public void pauseCycle() {
