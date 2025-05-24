4480e0c3b5d863bd573deccd3fda477236774a2b
Furkan Mete Çalış <calisfurkanmete@yandex.com>
2025-05-24 16:38:21 +0300

    plus

--- src/main/java/com/tsmteam/erubz214javasmarttraffic/MainUI.java
+++ src/main/java/com/tsmteam/erubz214javasmarttraffic/MainUI.java
@@ -10,7 +10,7 @@ import java.net.URL;
 public class MainUI extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        URL fxmlLocation = MainUI.class.getResource("traffic_ui_main.fxml");
+        URL fxmlLocation = MainUI.class.getResource("odev.fxml");
         System.out.println(fxmlLocation);
         FXMLLoader loader = new FXMLLoader(fxmlLocation);
         Scene scene = new Scene(loader.load(), 600, 600);
